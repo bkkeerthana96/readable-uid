56db794c06f16f6ec69f0d34debf8dfb2061bbc6
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-08-05 17:30:23 +0530

    Update uid-gen-test.cpp

--- uid-gen-test.cpp
+++ uid-gen-test.cpp
@@ -8,6 +8,11 @@ TEST(UID, UID_is_generated_within_length_limit) {
   ASSERT_TRUE(uid.length() > 0);
 }
 
+TEST(UID, UID_is_greater_than_length_limit) {
+  std::string uid = uidgen();
+  ASSERT_FALSE(uid.length() > 64);  
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
